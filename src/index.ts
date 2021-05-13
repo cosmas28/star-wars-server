f448c561575c3ea820892d32e5f095b69dd2b4d1
Cosmas Augustino Billa <augubilla100@gmail.com>
2021-05-13 15:36:47 +0300

    create schema

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,49 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
+import {ApolloServer, gql} from "apollo-server-express";
+
+const PEOPLE = [
+	{
+		name: "Luke Skywalker",
+		height: "172",
+		gender: "male",
+		mass: "77", 
+		homeworld: "http://swapi.dev/api/planets/1/",
+	},
+	{
+		name: "C-3PO",
+		height: "167",
+		gender: "n/a",
+		mass: "75",
+		homeworld: "http://swapi.dev/api/planets/1/",
+	},
+];
+
+const typeDefs = gql`
+	type Person {
+		name: String!
+		height: String
+		gender: String
+		mass: String 
+		homeworld: String
+	}
+
+	type Query {
+		people: [Person]!
+		person(name: String!): Person
+	}
+`;
+
+const resolvers = {
+	Query: {
+		people: () => PEOPLE,
+	}
+}
 
 const app = express();
-
 app.use(cors());
-
-app.get("/", (req: Request, res: Response) => {
-	res.status(200).send("Hello world");
-});
+const server = new ApolloServer({typeDefs, resolvers});
+server.applyMiddleware({app, path: "/"});
 
 app.listen(8000, () => {
 	console.log("server Started at Port, 8000")
